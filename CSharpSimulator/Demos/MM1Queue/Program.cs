ba0450e8245a25b4ee5ff8ee3c62fc4fbc401196
li-haobin <i@li-haobin.net>
2015-03-15 22:35:13 +0800

    use hashset to store historical load for higher efficiency

--- CSharpSimulator/Demos/MM1Queue/Program.cs
+++ CSharpSimulator/Demos/MM1Queue/Program.cs
@@ -11,21 +11,39 @@ namespace CSharpSimulator.Demos.MM1Queue
     {
         static void Main(string[] args)
         {
-            double arrivalRate = 10, serviceRate = 12;
-            Console.WriteLine("Seed\tAverage Count");
-            var timer = new Timer();
-            for (int seed = 0; seed < 20; seed++)
+            while (true)
             {
-                var simulation = new Simulation(TimeSpan.FromHours(1.0 / arrivalRate), TimeSpan.FromHours(1.0 / serviceRate), seed);
-                timer.Check();
-                simulation.Run(10000);
-                var executionTime = timer.Check();
-                var averageCount = simulation.CustomerEventRecorder.AverageCount("Arrival", "Departure");
+                Console.Clear();
+                Console.Write("Arrival Rate (per hour): ");
+                double arrivalRate = Convert.ToDouble(Console.ReadLine()); //10 
+                Console.Write("Service Rate (per hour): ");
+                double serviceRate = Convert.ToDouble(Console.ReadLine()); //12
+                Console.Write("Number of Events for each replication: ");
+                int nEvents = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Number of replications: ");
+                int nReplications = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("{0}\t{1}\t{2}\t{3}", seed, averageCount, executionTime.TotalMilliseconds, timer.Check().TotalMilliseconds);
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Seed\tAverageCount\tExecutionTime\tAnalysisTime");
+                Console.WriteLine("---------------------------------");
+                var timer = new Timer();
+                for (int seed = 0; seed < nReplications; seed++)
+                {
+                    var simulation = new Simulation(TimeSpan.FromHours(1.0 / arrivalRate), TimeSpan.FromHours(1.0 / serviceRate), seed);
+                    timer.Check();
+                    simulation.Run(nEvents);
+                    var executionTime = timer.Check();
+                    var averageCount = simulation.CustomerEventRecorder.AverageCount("Arrival", "Departure");
+
+                    Console.WriteLine("{0}\t{1:0.0000000}\t{2:0.0000000}\t{3:0.0000000}", 
+                        seed, averageCount, executionTime.TotalSeconds, timer.Check().TotalSeconds);
+                }
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Theoretical:\t{0}", arrivalRate / (serviceRate - arrivalRate));
+                Console.WriteLine("---------------------------------");
+                Console.Write("Press any key to continue...");
+                Console.ReadKey();
             }
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Theoretical:\t{0}", arrivalRate / (serviceRate - arrivalRate));
         }
     }
 }
--- CSharpSimulator/EventRecorder.cs
+++ CSharpSimulator/EventRecorder.cs
@@ -17,27 +17,27 @@ namespace CSharpSimulator
         private DESModel _desModel;
         private Dictionary<string, int> _eventIndices;
         private List<EventRecord> _recordList;
-        private List<T> _loadList;
+        private HashSet<T> _loadSet;
         public bool Active { get; set; }
         public EventRecorder(DESModel desModel)
         {
             _desModel = desModel;
             _eventIndices = new Dictionary<string, int>();
             _recordList = new List<EventRecord>();
-            _loadList = new List<T>();
+            _loadSet = new HashSet<T>();
             Active = true;
         }
         public void CheckIn(T load, string eventKey, bool consoleDisplay = false)
         {
             if (!Active) return;
-            if (!_loadList.Contains(load)) _loadList.Add(load);
+            if (!_loadSet.Contains(load)) _loadSet.Add(load);
             _recordList.Add(new EventRecord { StatusIndex = GetIndex(eventKey), Load = load, ClockTime = _desModel.ClockTime });
             if (consoleDisplay) Console.WriteLine("{0}: {1} of {2}.", _desModel.ClockTime, eventKey, load.ToString());
         }
         public Dictionary<DateTime, int> Counts(string countInEventKeys, string countOutEventKeys)
         {
             _recordList = _recordList.OrderBy(r => r.ClockTime).ToList();
-            List<T> counted = new List<T>();
+            var counted = new HashSet<T>();
             var timeSeries = new Dictionary<DateTime, int>();
             var countInIndices = countInEventKeys.Split(',').Select(s => GetIndex(s)).ToArray();
             var countOutIndices = countOutEventKeys.Split(',').Select(s => GetIndex(s)).ToArray();
