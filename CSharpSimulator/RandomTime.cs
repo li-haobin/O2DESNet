bef454b08cf3e14997f53581042c6e7a4450bf35
li-haobin <i@li-haobin.net>
2015-03-16 08:50:51 +0800

    uniform RandomTime

--- CSharpSimulator/RandomTime.cs
+++ CSharpSimulator/RandomTime.cs
@@ -9,10 +9,22 @@ namespace CSharpSimulator
 {
     public class RandomTime
     {
+        private static Random GetRS(Random rs)
+        {
+            if (rs == null) return new Random();
+            return rs;
+        }        
+        public static TimeSpan Uniform(TimeSpan max, Random rs = null)
+        {
+            return TimeSpan.FromMinutes(max.TotalMinutes * GetRS(rs).NextDouble());
+        }
+        public static TimeSpan Uniform(TimeSpan min, TimeSpan max, Random rs = null)
+        {
+            return TimeSpan.FromMinutes((max.TotalMinutes - min.TotalMinutes) * GetRS(rs).NextDouble() + min.TotalMinutes);
+        }
         public static TimeSpan Exponential(TimeSpan mean, Random rs = null)
         {
-            if (rs == null) rs = new Random();
-            return TimeSpan.FromMinutes(MathNet.Numerics.Distributions.Exponential.Sample(rs, 1.0 / mean.TotalMinutes));
+            return TimeSpan.FromMinutes(MathNet.Numerics.Distributions.Exponential.Sample(GetRS(rs), 1.0 / mean.TotalMinutes));
         }
     }
 }
