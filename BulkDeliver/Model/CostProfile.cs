c874fc9e495704ae85dbda504a6e1cd0e924a1de
Haobin Li <i@li-haobin.net>
2015-05-19 11:17:35 +0800

    Examples for cost and item type

--- BulkDeliver/Model/CostProfile.cs
+++ BulkDeliver/Model/CostProfile.cs
@@ -36,6 +36,64 @@ namespace BulkDeliver.Model
                 }).OrderBy(p => p.StartWeight).ToList()
             };
         }
+        public static CostProfile ExampleAirfreight
+        {
+            get
+            {
+                return GetCostProfile(1000, new List<double[]>
+                {
+                    new double[] { 200, 5 },
+                    new double[] { 1000, 4 },
+                    new double[] { 2000, 3.5 },
+                    new double[] { 3000, 3 }
+                });
+            }
+        }
+        public static CostProfile ExampleContainer
+        {
+            get
+            {
+                return GetCostProfile(0, new List<double[]>
+                {
+                    new double[] { 0, 8000000 },
+                    new double[] { 0.001, 0 },
+                    new double[] { 5000, 8000000 },
+                    new double[] { 5000.001, 0 }
+                });
+            }
+        }
+        public static CostProfile ExamplePallete
+        {
+            get
+            {
+                return GetCostProfile(2000, new List<double[]>
+                {
+                    new double[] { 0, 1840000 },
+                    new double[] { 0.001, 0 },
+                    new double[] { 1000, 1840000 },
+                    new double[] { 1000.001, 0 },
+                    new double[] { 2000, 1840000 },
+                    new double[] { 2000.001, 0 },
+                    new double[] { 3000, 1840000 },
+                    new double[] { 3000.001, 0 },
+                    new double[] { 4000, 1840000 },
+                    new double[] { 4000.001, 0 },
+                    new double[] { 5000, 1840000 },
+                    new double[] { 5000.001, 0 },
+                    new double[] { 6000, 1840000 },
+                    new double[] { 6000.001, 0 },
+                    new double[] { 7000, 1840000 },
+                    new double[] { 7000.001, 0 },
+                    new double[] { 8000, 1840000 },
+                    new double[] { 8000.001, 0 },
+                    new double[] { 9000, 1840000 },
+                    new double[] { 9000.001, 0 },
+                    new double[] { 10000, 1840000 },
+                    new double[] { 10000.001, 0 },
+                    new double[] { 11000, 1840000 }
+                });
+            }
+        }
     }
 
     public class PieceCost
--- BulkDeliver/Model/ItemType.cs
+++ BulkDeliver/Model/ItemType.cs
@@ -15,5 +15,25 @@ namespace BulkDeliver.Model
         public double Value_Mean { get; set; } // in dollar
         public double Value_Offset { get; set; } // in dollar
         public double DailyInventoryCostRatio { get; set; }
+        public static ItemType GetExample(bool highDemand, bool highWeight, bool highValue){
+            var itemType = new ItemType { DailyInventoryCostRatio = 0.001 };
+            if (highDemand) itemType.IAT_Expected = TimeSpan.FromDays(30.0 / 3000);
+            else itemType.IAT_Expected = TimeSpan.FromDays(30.0 / 200);
+            double lb, ub;
+            
+            // weight
+            if (highWeight) { lb = 0.5; ub = 3.0; }
+            else { lb = 10.0; ub = 30.0; }
+            itemType.Weight_Mean = (ub + lb) / 2;
+            itemType.Weight_Offset = (ub - lb) / 2;
+            
+            // value
+            if (highValue) { lb = 1000; ub = 3000; }
+            else { lb = 50; ub = 200; }
+            itemType.Value_Mean = (ub + lb) / 2;
+            itemType.Value_Offset = (ub - lb) / 2;
+
+            return itemType;
+        }
     }
 }
