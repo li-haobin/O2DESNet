0223c381ab7351220ead5206f9de64faca2479da
Haobin Li <i@li-haobin.net>
2015-10-09 09:20:48 +0800

    create template  project

---
+++ O2DESNet.Template/Events/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace O2DESNet.Template
+{
+    internal abstract class Event : IEvent
+    {
+        protected Simulator _sim { get; private set; }
+        protected Event(Simulator simulator) { _sim = simulator; }
+        public abstract void Invoke();
+    }
+}
---
+++ O2DESNet.Template/Statics/Scenario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O2DESNet.Template
+{
+    public class Scenario
+    {
+        // involve properties for all staitc Entity & Relationships
+
+        public Scenario() { }
+
+        // write help methods to construct a scenario
+
+        // encapsulate status-independent generating funcsions here
+    }
+}
