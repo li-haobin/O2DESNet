f7dce137c23be1455a83cc6011c178e47f83fedc
Vinsensius Albert <vinsensius.albert@u.nus.edu>
2015-10-26 11:24:29 +0800

    Created New Warehouse Project

---
+++ O2DESNet.Warehouse/Events/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace O2DESNet.Warehouse
+{
+    internal abstract class Event : IEvent
+    {
+        protected Simulator _sim { get; private set; }
+        protected Event(Simulator simulator) { _sim = simulator; }
+        public abstract void Invoke();
+        public abstract void Backtrack();
+    }
+}
