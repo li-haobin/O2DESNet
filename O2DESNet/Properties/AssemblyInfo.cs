c0a16518636efebbd6fd5cd351fb1f3d02aa8b70
Haobin Li <i@li-haobin.net>
2015-12-23 15:06:23 +0800

    change version number to v2.1

--- O2DESNet/Properties/AssemblyInfo.cs
+++ O2DESNet/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("O2DES.Net")]
-[assembly: AssemblyCopyright("Copyright © Dr. Haobin Li 2015")]
+[assembly: AssemblyCopyright("Copyright © PEDRIEL(LI)² 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
