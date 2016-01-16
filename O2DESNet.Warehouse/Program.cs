f3df4fc92443605aaae582fabb04bdd3a7f16eba
Vinsensius Albert <vinsensius.albert@u.nus.edu>
2016-01-16 23:35:30 +0800

    Simple Experiment Framework to select strategy

--- O2DESNet.Warehouse/Program.cs
+++ O2DESNet.Warehouse/Program.cs
@@ -21,21 +21,20 @@ namespace O2DESNet.Warehouse
             //    whsim.PrintStatistics();
             //}
 
-            WarehouseSim whsim = new WarehouseSim("ZA", PicklistGenerator.Strategy.A);
+            WarehouseSim whsim;
+
+            Console.Write("Strategy to implement? (A/B/C/D) ");
+            var strat = Console.ReadLine();
+
+            if (strat[0] == 'A' || strat[0] == 'a') whsim = new WarehouseSim("ZA", PicklistGenerator.Strategy.A);
+            else if (strat[0] == 'B' || strat[0] == 'b') whsim = new WarehouseSim("ZA", PicklistGenerator.Strategy.B);
+            else if (strat[0] == 'C' || strat[0] == 'c') whsim = new WarehouseSim("ZA", PicklistGenerator.Strategy.C);
+            else if (strat[0] == 'D' || strat[0] == 'd') whsim = new WarehouseSim("ZA", PicklistGenerator.Strategy.D);
+            else throw new Exception("No such strategy!");
+
             whsim.Run(24);
             whsim.PrintStatistics();
 
-            whsim = new WarehouseSim("ZA", PicklistGenerator.Strategy.B);
-            whsim.Run(24);
-            whsim.PrintStatistics();
-
-            whsim = new WarehouseSim("ZA", PicklistGenerator.Strategy.C);
-            whsim.Run(24);
-            whsim.PrintStatistics();
-
-            whsim = new WarehouseSim("ZA", PicklistGenerator.Strategy.D);
-            whsim.Run(24);
-            whsim.PrintStatistics();
 
             Console.WriteLine("\n:: Experiment End ::");
             Console.ReadKey();
