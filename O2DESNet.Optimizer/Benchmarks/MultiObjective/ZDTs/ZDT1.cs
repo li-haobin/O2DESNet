846769f0b5cbbfafd931b89d47e1874aea0b6108
Haobin Li <lihb@ihpc.a-star.edu.sg>
2016-11-22 15:35:01 -0700

    implement gradient calculation for ZDT1

--- O2DESNet.Optimizer/Benchmarks/MultiObjective/ZDTs/ZDT1.cs
+++ O2DESNet.Optimizer/Benchmarks/MultiObjective/ZDTs/ZDT1.cs
@@ -11,17 +11,34 @@ namespace O2DESNet.Optimizer.Benchmarks
     {
         public ZDT1(int nDecisions) : base() { NDecisions = nDecisions; }
         public override string ToString() { return "ZDT1"; }
-        public override DenseVector Evaluate(DenseVector decisions)
+        
+        #region Function Values
+        public override DenseVector Evaluate(DenseVector x)
         {
-            if (!FeasibilityCheck(decisions))
+            if (!FeasibilityCheck(x))
                 return new double[] { double.PositiveInfinity, double.PositiveInfinity };
-            int m = decisions.Count();
-            double f1, f2, g, h;
-            f1 = decisions.First();
-            g = 1.0; for (int i = 1; i < m; i++) g += 9 * decisions.ElementAt(i) / (m - 1);
-            h = 1 - Math.Sqrt(f1 / g);
-            f2 = g * h;
-            return new double[] { f1, f2 };
+            return new double[] { Vg1(x), Vg2(x) };
         }
+        private double Vg1(DenseVector x) { return x.First(); }
+        private double Vg2(DenseVector x) { return Vf(x) * Vh(x); }
+        private double Vf(DenseVector x)
+        {
+            double f = 1.0;
+            for (int i = 1; i < x.Count; i++) f += 9 * x[i] / (x.Count - 1);
+            return f;
+        }
+        private double Vh(DenseVector x) { return 1 - Math.Sqrt(Vg1(x) / Vf(x)); }
+        #endregion
+        
+        #region Derivatives
+        public override DenseMatrix Gradients(DenseVector x) { return DenseMatrix.OfRowVectors(new DenseVector[] { Dg1(x), Dg2(x) }); }
+        private DenseVector Dg1(DenseVector x) { var d = new double[x.Count]; d[0] = 1; return d; }
+        private DenseVector Dg2(DenseVector x) { return Vf(x) * Dh(x) + Vh(x) * Df(x); }
+        private DenseVector Df(DenseVector x) { var d = x.Select(v => 9.0 / (x.Count - 1)).ToArray(); d[0] = 0; return d; }
+        private DenseVector Dh(DenseVector x)
+        {
+            return 0.5 * (Math.Sqrt(Vg1(x) / Math.Pow(Vf(x), 3)) * Df(x) - Math.Sqrt(1 / (Vg1(x) * Vf(x))) * Dg1(x));
+        }
+        #endregion
     }
 }
