3b5cad95797b7d79af368e4c51d0ba58e2059785
Yuming Peng <isepy@nus.edu.sg>
2017-03-03 10:29:16 +0800

    add new project

---
+++ PMTest/PMTest/ControlPoint.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using O2DESNet;
+
+namespace Test
+{
+    public class ControlPoint : Component<ControlPoint.Statics>
+    {
+        #region Statics
+        public class Statics : Scenario
+        {
+            //public new Statics Config { get { return (Statics)base.Config; } } // for inheritated component
+
+            /// <summary>
+            /// The PathMover system it belongs to
+            /// </summary>
+            public PathMover.Statics PathMover { get; private set; }
+            //<summery>
+            //The index in the path mover system
+            //</summery>
+            public int Index { get; private set; }
+            /// <summary>
+            /// Check for the position on each path
+            /// </summary>
+            public Dictionary<Segment.Statics, double> Positions { get; internal set; } // have not well defined yet
+
+            public Statics(PathMover.Statics pathMover)
+            {
+                PathMover = pathMover;
+                Index = PathMover.ControlPoints.Count;
+                Positions = new Dictionary<Segment.Statics, double>();
+            }
+        }
+
+        #endregion
+
+        #region Sub-Components
+        //private Server<TLoad> Server { get; set; }              
+        #endregion
+
+        #region Dynamics
+        //public int Occupancy { get { return Server.Occupancy; } }  
+        public PathMover PathMover { get; internal set; }
+        public bool Locked { get; private set; } = false;// needed?
+        public Vehicle At { get; internal set; }
+        public HourCounter HourCounter { get; private set; }
+        internal HashSet<Vehicle> Outgoing { get; private set; }
+        internal HashSet<Vehicle> Incoming { get; private set; }
+        internal HashSet<Vehicle> Onposition { get; private set; }
+        /// <summary>
+        /// Check if the control point is accessible via the given path
+        /// </summary>
+        public bool Accessible() { return true; }// have not defined yet
+        internal List<Segment> IncomingSegments { get; private set; }
+        internal List<Segment> OutgoingSegments { get; private set; }
+        #endregion
+
+        #region Events
+        private abstract class EventOfControlPoint : Event { internal ControlPoint This { get; set; } } // event adapter 
+                                                                                                //private class InternalEvent : EventOfPath
+                                                                                                //{
+                                                                                                //    internal TLoad Load { get; set; }
+                                                                                                //    public override void Invoke() {  }
+                                                                                                //}
+        private class TravelToEvent : Event
+        {
+            public ControlPoint ControlPoint { get; private set; }
+            public Vehicle Vehicle { get; private set; }
+            internal TravelToEvent (ControlPoint controlpoint, Vehicle vehicle)
+            {
+                ControlPoint = controlpoint;
+                Vehicle = vehicle;
+            }
+            public override void Invoke() 
+            {
+                throw new NotImplementedException();
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+        private class TravelFromEvent : Event
+        {
+            public ControlPoint ControlPoint { get; private set; }
+            public Vehicle Vehicle { get; private set; }
+            internal TravelFromEvent (ControlPoint controlpoint, Vehicle vehicle)
+            {
+                ControlPoint = controlpoint;
+                Vehicle = vehicle;
+            }
+            public override void Invoke()
+            {
+                throw new NotImplementedException();
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+        private class ReachEvent : Event
+        {
+            public ControlPoint ControlPoint { get; private set; }
+            public Vehicle Vehicle { get; set; }
+            internal ReachEvent (ControlPoint controlpoint, Vehicle vehicle)
+            {
+                ControlPoint = controlpoint;
+                Vehicle = vehicle;
+            }
+            public override void Invoke()
+            {
+                throw new NotImplementedException();
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+        private class LeaveEvent : Event
+        {
+            public ControlPoint ControlPoint { get; private set; }
+            public Vehicle Vehicle { get; private set; }
+            internal LeaveEvent (ControlPoint controlpoint, Vehicle vehicle)
+            {
+                ControlPoint = controlpoint;
+                Vehicle = vehicle;
+            }
+            public override void Invoke()
+            {
+                throw new NotImplementedException();
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+
+        #endregion
+
+        #region Input Events - Getters
+        //public Event Input(TLoad load) { return new InternalEvent { This = this, Load = load }; }
+        public Event Reach(Vehicle vehicle) { return new ReachEvent(this, vehicle); }
+        public Event Leave(Vehicle vehicle) { return new LeaveEvent(this, vehicle); }
+        public Event TravelFrom(Vehicle vehicle) { return new TravelFromEvent(this, vehicle); }
+        public Event TravelTo(Vehicle vehicle) { return new TravelToEvent(this, vehicle); }
+        #endregion
+
+        #region Output Events - Reference to Getters
+        //public List<Func<TLoad, Event>> OnOutput { get; private set; } = new List<Func<TLoad, Event>>();
+        public List<Func<Event>> OnRelease { get; private set; } = new List<Func<Event>>();
+        #endregion
+
+        #region Exeptions
+        public class VehicleIsNotIncomingException : Exception
+        {
+            public VehicleIsNotIncomingException() : base("Please ensure the vehicle is incoming before execute 'Reach' event at the control point.") { }
+        }
+        public class VehicleIsNotAtException : Exception
+        {
+            public VehicleIsNotAtException() : base("Please ensure the vehicle is at the control point before execute 'StartMoveOut' event.") { }
+        }
+        public class VehicleIsNotOutgoingException : Exception
+        {
+            public VehicleIsNotOutgoingException() : base("Please ensure the vehicle is outgoing before execute 'Leave' event.") { }
+        }
+        public class ZeroVacancyException : Exception
+        {
+            public ZeroVacancyException() : base("Control Point is not accessible due to vacancy issue.") { }
+        }
+        #endregion
+
+        public ControlPoint(Statics config, int seed, string tag = null) : base(config, seed, tag)
+        {
+            Name = "ControlPoint";
+            At = null;
+            IncomingSegments = new List<Segment>();
+            OutgoingSegments = new List<Segment>();
+            HourCounter = new HourCounter();
+
+        }
+
+        public override void WarmedUp(DateTime clockTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PMTest/PMTest/Vehicle.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using O2DESNet;
+using static Test.ControlPoint;
+using Test;
+
+namespace Test
+{
+    public class Vehicle : Load<Vehicle.Statics>
+    {
+        #region Statics
+        public class Statics : Scenario
+        {
+            public string Name { get; set; }
+            public double Speed { get; set; }//static or dynamic?
+
+            /// <summary>
+            /// Timestamps are recorded for tracking the movement of the vehicle
+            /// </summary>
+            public bool KeepTrack { get; set; }
+            public double Accelerate { get; set; }// statics or dynamic?
+
+            public double Length { get; set; } = 3.95;
+            public double SafetyLength { get { return Length * (1 + DistanceSafetyFactor); } }
+            public double Width { get; set; } = 1.67;
+            public double DistanceSafetyFactor { get; set; } = 0.15;
+
+            public string Color { get; set; } = "green";
+
+            public Statics() { Name = Guid.NewGuid().ToString().ToUpper().Substring(0, 4); }
+        }
+        //public new Statics Config { get { return (Statics)base.Config; } } // for inheritated component
+
+        #endregion
+
+        #region Sub-Components
+        //private Server<TLoad> Server { get; set; }        
+        #endregion
+
+        #region Dynamics
+        //public int Occupancy { get { return Server.Occupancy; } }  
+        public PathMover PathMover { get; private set; }
+        public virtual double Speed { get; set; }
+        public virtual double Accelerate { get; }//have not set yet
+        public List<ControlPoint> Targets { get; private set; } = new List<ControlPoint>();
+        public ControlPoint Current { get; set; } = null;
+        public ControlPoint NextControlPoint { get; set; }//have not defined yet
+        public Segment SegmentToNextControlPoint { get;  }//have not defined yet
+        public Segment NextSegment { get; }//have not defined yet
+
+        public enum State { Travelling,Parking,Off }
+        public List<Tuple<double, State>> StateHistory { get; private set; } = new List<Tuple<double, State>> { new Tuple<double, State>(0, State.Off) };
+        public void LogState(DateTime clockTime, State state) { StateHistory.Add(new Tuple<double, State>((clockTime - PathMover.StartTime).TotalSeconds, state)); }
+        public void ResetStateHistory() { StateHistory = new List<Tuple<double, State>> { new Tuple<double, State>(0, StateHistory.Last().Item2) }; }
+        #endregion
+
+        #region Events
+        private abstract class EventOfVehicle : Event { internal Vehicle This { get; set; } } // event adapter 
+                                                                                              //private class InternalEvent : EventOfVehicle
+                                                                                              //{
+                                                                                              //    internal TLoad Load { get; set; }
+                                                                                              //    public override void Invoke() {  }
+                                                                                              //}
+
+        private class PutOnEvent : Event
+        {
+            public ControlPoint ControlPoint { get; private set; }
+            public Vehicle Vehicle { get; private set; }
+            internal PutOnEvent(ControlPoint controlPoint, Vehicle vehicle)
+            {
+                ControlPoint = controlPoint;
+                Vehicle = vehicle;
+                Vehicle.PathMover = ControlPoint.PathMover;
+            }
+            public override void Invoke() { }
+            
+            public override string ToString() { return string.Format("{0}_PutOn", Vehicle); }
+        }
+
+        private class PutOffEvent : Event
+        {
+            public Vehicle Vehicle { get; private set; }
+            internal PutOffEvent(Vehicle vehicle) { Vehicle = vehicle; }
+            public override void Invoke() { }
+            public override string ToString() { return string.Format("{0}_PutOff", Vehicle); }
+        }
+
+        private class MoveEvent : Event
+        {
+            public Vehicle Vehicle { get; private set; }
+            internal MoveEvent(Vehicle vehicle) { Vehicle = vehicle; }
+            public override void Invoke() { }
+            public override string ToString() { return string.Format("{0}_Move", Vehicle); }
+        }
+
+        private class ReachEvent : Event
+        {
+            public Vehicle Vehicle { get; private set; }
+            internal ReachEvent(Vehicle vehicle) { Vehicle = vehicle; }
+            public override void Invoke()
+            {
+                var next = Vehicle.NextControlPoint;
+                Vehicle.Log(this);
+                Execute(next.Reach(Vehicle));
+                Vehicle.Current = next;
+
+                while (Vehicle.Current == Vehicle.Targets.FirstOrDefault()) Vehicle.Targets.RemoveAt(0);
+            }
+            public override string ToString() { return string.Format("{0}_Reach", Vehicle); }
+        }
+
+        private class MoveToEvent : Event
+        {
+            public Vehicle Vehicle { get; private set; }
+            public List<ControlPoint> Targets { get; private set; }
+            internal MoveToEvent(Vehicle vehicle, List<ControlPoint> targets)
+            {
+                Vehicle = vehicle;
+                Targets = targets;
+            }
+            public override void Invoke() { }
+            public override string ToString() { return string.Format("{0}_MoveTo", Vehicle); }
+        }
+
+        private class ClearTargetsEvent : Event
+        {
+            public Vehicle Vehicle { get; private set; }
+            internal ClearTargetsEvent(Vehicle vehicle) { Vehicle = vehicle; }
+            public override void Invoke()
+            {
+                if (Vehicle.Targets.Count > 0) Vehicle.Targets = new List<ControlPoint> { Vehicle.Targets.First() };
+            }
+            public override string ToString() { return string.Format("{0}_ClearTargets", Vehicle); }
+        }
+
+        private class CompleteEvent : Event
+        {
+            public Vehicle Vehicle { get; private set; }
+            internal CompleteEvent(Vehicle vehicle) { Vehicle = vehicle; }
+            public override void Invoke()
+            {
+                Vehicle.LogState(ClockTime, Vehicle.State.Parking);
+                foreach (var evnt in Vehicle.OnComplete) Execute(evnt());
+            }
+            public override string ToString() { return string.Format("{0}_Complete", Vehicle); }
+        }
+
+        private class UpdatePositionsInSegmentEvent : Event
+        {
+            public Vehicle Vehicle { get; private set; }
+            public UpdatePositionsInSegmentEvent(Vehicle vehicle) { Vehicle = vehicle; }
+            public override void Invoke() { }
+        }
+
+
+        #endregion
+
+        #region Input Events - Getters
+        //public Event Input(TLoad load) { return new InternalEvent { This = this, Load = load }; }
+        public Event PutOn(ControlPoint current) { return new PutOnEvent(current, this); }
+        public Event PutOff() { return new PutOffEvent(this); }
+        /// <summary>
+        /// Move the vehicle to a list of targets in sequence, append to the list if there are existing targets
+        /// </summary>
+        public Event MoveTo(List<ControlPoint> targets) { return new MoveToEvent(this, targets); }
+        /// <summary>
+        /// Clear all targets except the first one which is being executed.
+        /// </summary>
+        public Event ClearTargets() { return new ClearTargetsEvent(this); }
+
+        // Moving from control point to control point
+        internal Event Move() { return new MoveEvent(this); }
+        internal Event Reach() { return new ReachEvent(this); }
+        internal Event Complete() { return new CompleteEvent(this); }
+        internal Event UpdatePositionsInSegment() { return new UpdatePositionsInSegmentEvent(this); }
+        #endregion
+
+        #region Output Events - Reference to Getters
+        //public List<Func<TLoad, Event>> OnOutput { get; private set; } = new List<Func<TLoad, Event>>();
+        public List<Func<Event>> OnComplete { get; private set; } = new List<Func<Event>>();
+        #endregion
+
+        #region Exeptions
+        public class VehicleStatusException : Exception
+        {
+            public VehicleStatusException(string message) : base(string.Format("Vechile Status Exception: {0}", message)) { }
+        }
+        #endregion
+
+        public Vehicle(Statics config, int seed, string tag = null) : base(config, seed, tag)
+        {
+            Name = "Vehicle";
+        }
+
+        public override void WarmedUp(DateTime clockTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Log(Event evnt) { }
+
+        public override void WriteToConsole(DateTime? clockTime) { }
+    }
+}
