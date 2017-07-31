352fcef786bec67d083330aa6fd6d40354fcddfb
Haobin Li <i@li-haobin.net>
2017-07-31 10:16:52 +0800

    - Version 2.4.1 Include Uniform and Exponential for the random generators.

---
+++ O2DESNet/Distributions/Exponential.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace O2DESNet.Distributions
+{
+    public static class Exponential
+    {
+        public static double Sample(Random rs, double mean)
+        {
+            return MathNet.Numerics.Distributions.Exponential.Sample(rs, 1 / mean);
+        }
+
+        public static double CDF(double mean, double x)
+        {
+            return MathNet.Numerics.Distributions.Exponential.CDF(1 / mean, x);
+        }
+
+        public static double InvCDF(double mean, double p)
+        {
+            return MathNet.Numerics.Distributions.Exponential.InvCDF(1 / mean, p);
+        }
+
+        public static TimeSpan Sample(Random rs, TimeSpan mean)
+        {
+            return TimeSpan.FromDays(Sample(rs, mean.TotalDays));
+        }
+    }
+}
---
+++ O2DESNet/Distributions/Uniform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace O2DESNet.Distributions
+{
+    public static class Uniform
+    {
+        public static double Sample(Random rs, double lowerbound, double upperbound)
+        {
+            return lowerbound + (upperbound - lowerbound) * rs.NextDouble();
+        }
+
+        public static TimeSpan Sample(Random rs, TimeSpan lowerbound, TimeSpan upperbound)
+        {
+            return TimeSpan.FromDays(Sample(rs, lowerbound.Days, upperbound.Days));
+        }
+
+        public static T Sample<T>(Random rs, IEnumerable<T> candidates)
+        {
+            if (candidates.Count() == 0) return default(T);
+            return candidates.ElementAt(rs.Next(candidates.Count()));
+        }
+    }
+}
