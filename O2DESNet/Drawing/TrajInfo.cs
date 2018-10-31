7c1f1b230d2e2a308adcea83c2d2e8000968f551
Li Haobin <li_haobin@nus.edu.sg>
2018-10-31 23:18:13 +0800

    create class for trajectory information

---
+++ O2DESNet/Drawing/TrajInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O2DESNet.Drawing
+{
+    /// <summary>
+    /// The object passed by module to update trajectory of its movement
+    /// </summary>
+    public class TrajInfo
+    {
+        /// <summary>
+        /// Whether to rewrite previous trjectory information.
+        /// Set to false by default.
+        /// </summary>
+        public bool Rewrite { get; set; } = false;
+        /// <summary>
+        /// The trajectory that consists of a list of spatio temporal points
+        /// </summary>
+        public List<SpatioTemporalPoint> Trajectory { get; set; }
+    }
+    public class SpatioTemporalPoint
+    {
+        public DateTime Time { get; set; }
+        public double? X { get; set; } = null;
+        public double? Y { get; set; } = null;
+        public double? Z { get; set; } = null;
+    }
+}
